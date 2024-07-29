5b87f552bd1645a7e90a374e387ead6480b7126e
Alex Nesterovich <a.v.nesterovich@gmail.com>
2024-07-29 02:09:09 +0200

    Refactor UI. Split chat and history. Add test data generation to do not call API

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 import pandas as pd
 import streamlit as st
-from langchain_openai import OpenAI
-from data.csv_loader import DataLoaderLocalCsv
+
+from ai.agent import RealEstateGPT
 from common.cfg import *
+from data.csv_loader import DataLoaderLocalCsv
+
 
 @st.cache_data
 def load_data():
@@ -10,46 +12,39 @@ def load_data():
     df_data = dataloader.df_formatted
     return df_data
 
-# Load data using the cached function
 df_data = load_data()
-df_header = df_data.columns.tolist()
 
-def generate_response(input_text, api_key):
-    llm = OpenAI(temperature=0, openai_api_key=api_key)
-    res = llm.invoke(input_text)
-    st.info(res)
-    return res
+if 'conversation_history' not in st.session_state:
+    st.session_state['conversation_history'] = []
+if 'ai_agent' not in st.session_state:
+    st.session_state['ai_agent'] = RealEstateGPT(df_data)
+
+def generate_response(input_text, use_test_data):
+    process_query(input_text, use_test_data)
+    st.rerun()
 
 def fix_dataframe(df):
-    # Iterate over each column in the DataFrame
     for column in df.columns:
-        # Convert each column to string type
         df[column] = df[column].astype(str)
     return df
 
 @st.cache_data
 def display_filters():
-    # Prepare data for horizontal display
     rows = []
-    max_sample_size = 3  # Number of sample values to show
+    max_sample_size = 3
 
     for col in df_data.columns:
         unique_values = df_data[col].unique()
-        # Limit the number of values to display
         sample_values = unique_values[:max_sample_size]
-        # Fill up with empty strings if fewer values than max_sample_size
         sample_values = list(sample_values) + [''] * (max_sample_size - len(sample_values))
         rows.append([col] + sample_values)
 
-    # Define columns based on the number of sample values
     columns = ["Header"] + [f"Value {i+1}" for i in range(max_sample_size)]
-
-    # Convert to DataFrame for display
     df_summary = pd.DataFrame(rows, columns=columns)
 
     st.write("Here are the column headers and sample values:")
     df_summary_fixed = fix_dataframe(df_summary)
-    st.table(df_summary_fixed)  # Display as a table
+    st.table(df_summary_fixed)
 
 def display_api_key():
     st.markdown(
@@ -59,8 +54,7 @@ def display_api_key():
     )
     st.write('___')
     st.markdown(
-        'Enter ['
-        'OpenAI API Key](https://platform.openai.com/account/api-keys) * optional',
+        'Enter [OpenAI API Key](https://platform.openai.com/account/api-keys) * optional',
         unsafe_allow_html=True
     )
 
@@ -69,10 +63,24 @@ def display_api_key():
     )
     return openai_api_key
 
-def execute():
-    st.title('🦾 AI real estate assistant Poland 🇵🇱')
+def process_query(query, use_test_data):
+    if query:
+        response = 'FAKE: '
+        if use_test_data:
+            response += fake_en.text(max_nb_chars=100)
+        else:
+            response = st.session_state['ai_agent'].ask_qn(query)
+        st.session_state['conversation_history'].insert(0, {'Client': query, 'AI': response})
 
-    # Custom CSS to style the layout
+def display_conversation():
+    if st.session_state['conversation_history']:
+        for idx, exchange in enumerate(st.session_state['conversation_history'], start=1):
+            st.text_area(f"Client ({idx})", value=exchange['Client'], height=100, disabled=True, key=f"client_{idx}")
+            st.text_area(f"AI ({idx})", value=exchange['AI'], height=100, disabled=True, key=f"ai_{idx}")
+
+def execute():
+    st.title('🦾 AI Real Estate Assistant Poland 🇵🇱')
+
     st.markdown("""
         <style>
         .full-width-form {
@@ -92,8 +100,8 @@ def execute():
         }
         .button-container {
             display: flex;
-            align-items: center; /* Align items vertically center */
-            gap: 10px; /* Adjust space between elements */
+            align-items: center;
+            gap: 10px;
             margin-bottom: 20px;
         }
         .form-container {
@@ -102,56 +110,64 @@ def execute():
         .api-key-container {
             flex: 1;
         }
+        .conversation-container {
+            max-height: 90vh; /* Adjust to fill more of the screen */
+            # width: 100%;
+            # overflow-y: auto;
+        }
         </style>
     """, unsafe_allow_html=True)
 
-    # Create a container for buttons and form
-    col1, col2 = st.columns([1, 3])
+    col1, col2 = st.columns([2, 2])  # Adjust column widths as needed
+    # col1, col2 = st.columns([3, 1])  # Adjust column widths as needed
 
     with col1:
+        st.write("### Input and Settings")
+
         if st.button("Filters", key="filters"):
             st.session_state.show_filters = not st.session_state.get('show_filters', False)
 
-    with col2:
         if st.button("OpenAI API Key", key="api_key"):
             st.session_state.show_api_key = not st.session_state.get('show_api_key', False)
 
-    # Display the sections based on button clicks
-    if st.session_state.get('show_filters', False):
-        display_filters()
+        if st.session_state.get('show_filters', False):
+            display_filters()
 
-    if st.session_state.get('show_api_key', False):
-        openai_api_key = display_api_key()
-    else:
-        openai_api_key = None
+        if st.session_state.get('show_api_key', False):
+            openai_api_key = display_api_key()
+        else:
+            openai_api_key = None
 
-    # Create a form with a unique key
-    st.markdown('<div class="form-container">', unsafe_allow_html=True)
+        use_test_data = st.checkbox('Use Test Responses', value=True, key='use_test_data')
 
-    with st.form(key='full-width-form'):
-        # Input for user message
-        label = 'Talk to me about your dream house 😎:\n'
-        msg_example = (
-            'I am finding a cheap flat in Krakow.\n'
-            'Better to have 2 rooms, not 1st floor, more than 30 square meters, with garage and storage room, '
-            'also I would like to negotiate the final price.\n'
-            'Do you have options for me?'
-        )
-        text = st.text_area(label, msg_example, height=300)  # Adjust height as needed
+        with st.form(key='full-width-form'):
+            label = 'Talk to me about your dream property 😎:\n'
 
-        # Submit button
-        submitted = st.form_submit_button('Submit')
-        key = OPENAI_API_KEY
-        if submitted:
-            # Ensure key starts with 'sk-' before proceeding
-            if openai_api_key:
-                if not openai_api_key.startswith('sk-'):
-                    st.warning('Please enter a valid OpenAI API key starting with "sk-".', icon='⚠')
-                else:
-                    key = openai_api_key
+            conversation_history = st.session_state.get('conversation_history', [])
+            msg_example = '' if conversation_history else (
+                'I am finding a cheap flat in Krakow.\n'
+                'Better to have 2 rooms, not 1st floor, more than 30 square meters, with garage and storage room, '
+                'also I would like to negotiate the final price.\n'
+                'Do you have options for me?'
+            )
+            text = st.text_area(label=label, value=msg_example, height=200)
 
-            generate_response(text, key)
+            submitted = st.form_submit_button('Submit')
+            key = OPENAI_API_KEY
+            if submitted:
+                if openai_api_key:
+                    if not openai_api_key.startswith('sk-'):
+                        st.warning('Please enter a valid OpenAI API key starting with "sk-".', icon='⚠')
+                    else:
+                        key = openai_api_key
 
-    st.markdown('</div>', unsafe_allow_html=True)
+                generate_response(text, use_test_data)
+
+    with col2:
+        st.write("### Conversation History")
+        with st.container():
+            st.markdown('<div class="conversation-container">', unsafe_allow_html=True)
+            display_conversation()
+            st.markdown('</div>', unsafe_allow_html=True)
 
 execute()
